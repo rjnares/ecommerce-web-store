9ae7d580e7ea2b65ac203e69ec18cef8858fe35a
rjnares <roberto.nares@gmail.com>
2021-03-23 22:45:38 -0700

    Created cart layout; needs routing to navigate to products page

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+
+import useStyles from "./styles";
+import CardItem from "./CartItem/CartItem";
+
+const Cart = ({ cart }) => {
+  const classes = useStyles();
+
+  const EmptyCart = () => (
+    <Typography variant="subtitle1">
+      Your shopping cart is empty. Go buy stuff!
+    </Typography>
+  );
+
+  const FilledCart = () => (
+    <React.Fragment>
+      <Grid container spacing={3}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <CardItem item={item} />
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cartDetails}>
+        <Typography variant="h4">
+          Subtotal: {cart.subtotal.formatted_with_symbol}
+        </Typography>
+        <div>
+          <Button
+            className={classes.emptyButton}
+            type="button"
+            size="large"
+            variant="contained"
+            color="secondary"
+          >
+            Empty Cart
+          </Button>
+          <Button
+            className={classes.checkoutButton}
+            type="button"
+            size="large"
+            variant="contained"
+            color="primary"
+          >
+            Checkout
+          </Button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+
+  if (!cart.line_items) return "Loading...";
+
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant="h3" gutterBottom>
+        Your Shopping Cart
+      </Typography>
+      {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
+export default Cart;
---
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+
+import useStyles from "./styles";
+
+const CartItem = ({ item }) => {
+  const classes = useStyles();
+
+  return (
+    <Card>
+      <CardMedia
+        image={item.media.source}
+        alt={item.name}
+        className={classes.media}
+      ></CardMedia>
+      <CardContent className={classes.cardContent}>
+        <Typography variant="h4">{item.name}</Typography>
+        <Typography variant="h5">
+          {item.line_total.formatted_with_symbol}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <div className={classes.buttons}>
+          <Button type="button" size="small">
+            -
+          </Button>
+          <Typography>{item.quantity}</Typography>
+          <Button type="button" size="small">
+            +
+          </Button>
+        </div>
+        <Button variant="contained" type="button" color="secondary">
+          Remove
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default CartItem;
