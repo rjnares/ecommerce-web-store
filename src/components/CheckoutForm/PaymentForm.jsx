4a77953da8240bd8b9a337bab2c7bf07ef9f62a2
rjnares <roberto.nares@gmail.com>
2021-03-24 18:58:24 -0700

    Added order summary to checkout > payment details

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import {
+  Elements,
+  CardElement,
+  ElementsConsumer,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-const PaymentForm = ({ shippingData }) => {
-  return <div>PaymentForm</div>;
+import Review from "./Review";
+
+const PaymentForm = ({ shippingData, checkoutToken }) => {
+  return (
+    <React.Fragment>
+      <Review checkoutToken={checkoutToken} />
+    </React.Fragment>
+  );
 };
 
 export default PaymentForm;
---
+++ src/components/CheckoutForm/Review.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const Review = ({ checkoutToken }) => {
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Order Summary
+      </Typography>
+      <List disablePadding>
+        {checkoutToken.live.line_items.map((product) => (
+          <ListItem styles={{ padding: "10px 0" }} key={product.name}>
+            <ListItemText
+              primary={product.name}
+              secondary={`Quantity: ${product.quantity}`}
+            />
+            <Typography variant="body2">
+              {product.line_total.formatted_with_symbol}
+            </Typography>
+          </ListItem>
+        ))}
+        <ListItem styles={{ padding: "10px 0" }}>
+          <ListItemText primary="Total" />
+          <Typography variant="subtitle1" style={{ fontWeight: 700 }}>
+            {checkoutToken.live.subtotal.formatted_with_symbol}
+          </Typography>
+        </ListItem>
+      </List>
+    </React.Fragment>
+  );
+};
+export default Review;
